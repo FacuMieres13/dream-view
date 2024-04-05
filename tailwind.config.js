6ec0818c66194ebf9cb003ddff575ebbd7179786
FacuMieres13 <aguscora00@gmail.com>
2024-04-05 08:53:14 -0300

    fixing bug on responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     screens: {
-      'mb': '350px',
+      'mb': '320px',
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
